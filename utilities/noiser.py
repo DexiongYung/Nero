0621ffde0acf2884bd1ff44c071f68f25eefe3de
Dylan Yung <YungDexiong@hotmail.com>
2020-03-25 23:53:31 -0700

    fixed noiser

--- utilities/noiser.py
+++ utilities/noiser.py
@@ -5,8 +5,8 @@ from random import randint
 
 CHARACTER_REPLACEMENT = dict()
 CHARACTER_REPLACEMENT['A'] = 'QSZWXa'
-CHARACTER_REPLACEMENT['B'] = 'NHGVb '
-CHARACTER_REPLACEMENT['C'] = 'VFDXc '
+CHARACTER_REPLACEMENT['B'] = 'NHGVb'
+CHARACTER_REPLACEMENT['C'] = 'VFDXc'
 CHARACTER_REPLACEMENT['D'] = 'FRESXCd'
 CHARACTER_REPLACEMENT['E'] = 'SDFR$#WSe'
 CHARACTER_REPLACEMENT['F'] = 'GTRDCVf'
@@ -16,8 +16,8 @@ CHARACTER_REPLACEMENT['I'] = 'UJKLO(*i'
 CHARACTER_REPLACEMENT['J'] = 'MKIUYHNj'
 CHARACTER_REPLACEMENT['K'] = 'JM<LOIk'
 CHARACTER_REPLACEMENT['L'] = 'K<>:POl'
-CHARACTER_REPLACEMENT['M'] = 'NJK< m'
-CHARACTER_REPLACEMENT['N'] = 'BHJM n'
+CHARACTER_REPLACEMENT['M'] = 'NJK<m'
+CHARACTER_REPLACEMENT['N'] = 'BHJMn'
 CHARACTER_REPLACEMENT['O'] = 'PLKI()Po'
 CHARACTER_REPLACEMENT['P'] = 'OL:{_)O"p'
 CHARACTER_REPLACEMENT['Q'] = 'ASW@!q'
@@ -25,14 +25,14 @@ CHARACTER_REPLACEMENT['R'] = 'TFDE$r%'
 CHARACTER_REPLACEMENT['S'] = 'DXZAWEs'
 CHARACTER_REPLACEMENT['T'] = 'YGFR%^t'
 CHARACTER_REPLACEMENT['U'] = 'IJHY&*u'
-CHARACTER_REPLACEMENT['V'] = 'CFGB v'
+CHARACTER_REPLACEMENT['V'] = 'CFGBv'
 CHARACTER_REPLACEMENT['W'] = 'SAQ@#Ew'
 CHARACTER_REPLACEMENT['X'] = 'ZASDCx'
 CHARACTER_REPLACEMENT['Y'] = 'UGHT^&y'
 CHARACTER_REPLACEMENT['Z'] = 'XSAz'
 CHARACTER_REPLACEMENT['a'] = 'qwszA'
-CHARACTER_REPLACEMENT['b'] = 'nhgv B'
-CHARACTER_REPLACEMENT['c'] = 'vfdx C'
+CHARACTER_REPLACEMENT['b'] = 'nhgvB'
+CHARACTER_REPLACEMENT['c'] = 'vfdxC'
 CHARACTER_REPLACEMENT['d'] = 'fresxcD'
 CHARACTER_REPLACEMENT['e'] = 'sdfr43wsE'
 CHARACTER_REPLACEMENT['f'] = 'gtrdcvF'
@@ -42,8 +42,8 @@ CHARACTER_REPLACEMENT['i'] = 'ujklo98I'
 CHARACTER_REPLACEMENT['j'] = 'mkiuyhnJ'
 CHARACTER_REPLACEMENT['k'] = 'jm,loijK'
 CHARACTER_REPLACEMENT['l'] = 'k,.;pokL'
-CHARACTER_REPLACEMENT['m'] = 'njk, M'
-CHARACTER_REPLACEMENT['n'] = 'bhjm N'
+CHARACTER_REPLACEMENT['m'] = 'njk,M'
+CHARACTER_REPLACEMENT['n'] = 'bhjmN'
 CHARACTER_REPLACEMENT['o'] = 'plki90pO'
 CHARACTER_REPLACEMENT['p'] = 'ol;[-0oP'
 CHARACTER_REPLACEMENT['q'] = 'asw21Q'
@@ -51,7 +51,7 @@ CHARACTER_REPLACEMENT['r'] = 'tfde45R'
 CHARACTER_REPLACEMENT['s'] = 'dxzaweS'
 CHARACTER_REPLACEMENT['t'] = 'ygfr56T'
 CHARACTER_REPLACEMENT['u'] = 'ijhy78U'
-CHARACTER_REPLACEMENT['v'] = 'cfgb V'
+CHARACTER_REPLACEMENT['v'] = 'cfgbV'
 CHARACTER_REPLACEMENT['w'] = 'saq23eW'
 CHARACTER_REPLACEMENT['x'] = 'zsdcX'
 CHARACTER_REPLACEMENT['y'] = 'uhgt67Y'
@@ -82,7 +82,7 @@ def noise_name(x: str, allowed_chars: str, max_noise: int = 2):
     elif noise_type == 3:
         ret = switch_to_similar(x, allowed_chars, max_switch=max_noise)
     
-    return ret.replace(' ', '')
+    return ret
 
 
 def noise_seperator(allowed_chars: str, x: str = " ", max_noise: int = 3):
@@ -165,4 +165,4 @@ def remove_chars(x: str, max_remove: int):
         pos = distributions.Categorical(torch.tensor([1 / x_length] * x_length)).sample().item()
         ret = "".join((ret[:pos], ret[pos + 1:]))
 
-    return ret.replace(" ", '')
+    return ret
